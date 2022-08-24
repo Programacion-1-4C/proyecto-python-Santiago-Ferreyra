d91696e2c69cc4934c5e52963df794e066c2f45f
Santiago-Ferreyra <110986619+Santiago-Ferreyra@users.noreply.github.com>
2022-08-24 11:13:59 -0300

    Add files via upload

--- proyecto_de_programacion.py
+++ proyecto_de_programacion.py
@@ -1,22 +1,23 @@
 import random
 
 
-
 def adiv_compu(x):
     bajo = 1
     alto = x
-    dev=''
+    dev = ''
     while dev != "c":
         if bajo != alto:
-            adiv = random.randint(bajo,alto)
+            adiv = random.randint(bajo, alto)
         else:
             adiv = bajo
-        adiv = random.randint(bajo,alto)
-        dev=input(f'es {adiv} muy alto(a),muy bajo(b) o correcto(c)').lower()
+        adiv = random.randint(bajo, alto)
+        dev = input(f'es {adiv} muy alto(a),muy bajo(b) o correcto(c)').lower()
         if dev == "a":
             alto = adiv - 1
         elif dev == "b":
             bajo = adiv + 1
     print("la computadora adivino tu numero correctamente")
 
-adiv_compu(100)
+
+if __name__ == "__main__":
+    adiv_compu(100)
