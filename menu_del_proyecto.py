ea1740199367b18d873e9c219d455dc7cce9cc03
Santiago-Ferreyra <110986619+Santiago-Ferreyra@users.noreply.github.com>
2022-08-31 00:13:38 -0300

    Add files via upload
    proyecto terminado

--- menu_del_proyecto.py
+++ menu_del_proyecto.py
@@ -1,16 +1,15 @@
-import random
-import math
 from proyecto_2 import adivina
 from proyecto_de_programacion import adiv_compu
 from pr_pa_ti import pr_pa_ti2
-from ahorcado import ahorcadopro
+from ahorcado import ahorcadohola
 while True:
 
     print(
-        "1. \n"
-        "2. \n"
-        "3. \n"
-        "*. Salir"
+        "1. adivina un numero que te da la compu \n"
+        "2.  piensa un numero y la compu lo adivinara \n"
+        "3. piedra papel o tijera \n"
+        "4. ahorcado \n"
+        "5. Salir"
      )
     option = int(input(">>> "))
     if option == 1:
@@ -20,132 +19,7 @@ while True:
     elif option == 3:
         pr_pa_ti2()
     elif option == 4:
-        AHORCADO = ['''
-                  +---+
-                  |   |
-                      |
-                      |
-                      |
-                      |
-                =========''', '''
-                  +---+
-                  |   |
-                  O   |
-                      |
-                      |
-                      |
-                =========''', '''
-                  +---+
-                  |   |
-                  O   |
-                  |   |
-                      |
-                      |
-                =========''', '''
-                  +---+
-                  |   |
-                  O   |
-                 /|   |
-                      |
-                      |
-                =========''', '''
-                  +---+
-                  |   |
-                  O   |
-                 /|\  |
-                      |
-                      |
-                =========''', '''
-                  +---+
-                  |   |
-                  O   |
-                 /|\  |
-                 /    |
-                      |
-                =========''', '''
-                  +---+
-                  |   |
-                  O   |
-                 /|\  |
-                 / \  |
-                      |
-                =========''']
-        palabras = 'valoracion aprenderpython comida juego python web imposible variable curso volador cabeza reproductor mirada escritor billete lapicero celular valor revista gratuito disco voleibol anillo estrella'.split()
+        ahorcadohola()
+    elif option == 5:
+        exit()
 
-
-        def buscarPalabraAleat(listaPalabras):
-            palabraAleatoria = random.randint(0, len(listaPalabras) - 1)
-            return listaPalabras[palabraAleatoria]
-
-
-        def displayBoard(AHORCADO, letraIncorrecta, letraCorrecta, palabraSecreta):
-            print(AHORCADO[len(letraIncorrecta)])
-            print("")
-            fin = " "
-            print('Letras incorrectas:', fin)
-            for letra in letraIncorrecta:
-                print(letra, fin)
-            print("")
-            espacio = '_' * len(palabraSecreta)
-            for i in range(len(palabraSecreta)):
-                if palabraSecreta[i] in letraCorrecta:
-                    espacio = espacio[:i] + palabraSecreta[i] + espacio[i + 1:]
-            for letra in espacio:
-                print(letra, fin)
-            print("")
-
-
-        def elijeLetra(algunaLetra):
-            while True:
-                print('Adivina una letra:')
-                letra = input()
-                letra = letra.lower()
-                if len(letra) != 1:
-                    print('Introduce una sola letra.')
-                elif letra in algunaLetra:
-                    print('Ya has elegido esa letra ¿Qué tal si pruebas con otra?')
-                elif letra not in 'abcdefghijklmnopqrstuvwxyz':
-                    print('Elije una letra.')
-                else:
-                    return letra
-
-
-        def empezar():
-            print('Quieres jugar de nuevo? (Si o No)')
-            return input().lower().startswith('s')
-
-
-        print('A H O R C A D O')
-        letraIncorrecta = ""
-        letraCorrecta = ""
-        palabraSecreta = buscarPalabraAleat(palabras)
-        finJuego = False
-        while True:
-            displayBoard(AHORCADO, letraIncorrecta, letraCorrecta, palabraSecreta)
-            letra = elijeLetra(letraIncorrecta + letraCorrecta)
-            if letra in palabraSecreta:
-                letraCorrecta = letraCorrecta + letra
-                letrasEncontradas = True
-                for i in range(len(palabraSecreta)):
-                    if palabraSecreta[i] not in letraCorrecta:
-                        letrasEncontradas = False
-                        break
-                if letrasEncontradas:
-                    print('¡Muy bien! La palabra secreta es "' + palabraSecreta + '"! ¡Has ganado!')
-                    finJuego = True
-            else:
-                letraIncorrecta = letraIncorrecta + letra
-                if len(letraIncorrecta) == len(AHORCADO) - 1:
-                    displayBoard(AHORCADO, letraIncorrecta, letraCorrecta, palabraSecreta)
-                    print('¡Se ha quedado sin letras!\nDespues de ' + str(
-                        len(letraIncorrecta)) + ' letras erroneas y ' + str(
-                        len(letraCorrecta)) + ' letras correctas, la palabra era "' + palabraSecreta + '"')
-                    finJuego = True
-            if finJuego:
-                if empezar():
-                    letraIncorrecta = ""
-                    letraCorrecta = ""
-                    finJuego = False
-                    palabraSecreta = buscarPalabraAleat(palabras)
-                else:
-                    break
